fb299af9aa32a1cd411da240e72552280c75650c
Shivangi Maurya <79085511+winterbear2000@users.noreply.github.com>
2021-07-09 00:31:53 +0530

    Add files via upload

---
+++ Day 14/script.js
@@ -0,0 +1,19 @@
+function func1(){
+    document.getElementById("one").innerHTML="Hello";
+}
+function func2(){
+    var n = document.getElementById("fname");
+    n.value = n.value.toUpperCase();
+}
+function func3()
+{
+    document.getElementById("two").innerHTML="You double clicked on the button";
+}
+function func4(){
+    document.getElementById("three").style.backgroundColor="yellow";
+    document.getElementById("three").innerHTML="How are you";
+}
+function func5(){
+    document.getElementById("three").style.backgroundColor="aqua";
+    document.getElementById("three").innerHTML="Hello";
+}

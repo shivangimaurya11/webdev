b83c2f41fc792bbce7c8f6f8ef9276e4f7d6311e
Shivangi Maurya <79085511+winterbear2000@users.noreply.github.com>
2021-07-13 18:39:54 +0530

    Add files via upload

---
+++ Day 18/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./components/Card";
+import "./style.css";
+function Navbar(){
+    return<div><h1> Navbar</h1></div>;
+}
+function App(){
+return(
+    <div className="App">
+    <Card></Card>
+       
+    </div>
+);
+}
+export default App;

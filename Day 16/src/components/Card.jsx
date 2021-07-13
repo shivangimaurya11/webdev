0fe4e91097d690efd82cec767a3eba217b167c97
Shivangi Maurya <79085511+winterbear2000@users.noreply.github.com>
2021-07-13 15:28:32 +0530

    Add files via upload

---
+++ Day 16/src/components/Card.jsx
@@ -0,0 +1,11 @@
+const Card=()=>
+{
+    return(
+        <div className="card">
+        <img src="https://th.bing.com/th/id/R.94d33d7323782fa173a75177e13dbb0e?rik=2tzoDk7ImpeaQQ&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fc%2f0%2f3%2f787450-new-death-note-l-wallpaper-1920x1080-for-lockscreen.jpg&ehk=Rf8bYt1J%2fMFJMvXpsrxFr3F6QP89g1weOF1TI15asio%3d&risl=&pid=ImgRaw" alt=""></img>
+            <h2>React Card</h2>
+            <p>This is a card</p>
+        </div>
+    );
+};
+export default Card;

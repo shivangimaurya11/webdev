248704b9ce09a3e2d0292eb5649f24efc7152c23
Shivangi Maurya <79085511+winterbear2000@users.noreply.github.com>
2021-07-13 19:08:09 +0530

    Update App.jsx

--- tha 19/App.jsx
+++ tha 19/App.jsx
@@ -1,14 +1,39 @@
-import Card from "./components/Card";
-import "./style.css";
-function Navbar(){
-    return<div><h1> Navbar</h1></div>;
-}
-function App(){
-return(
-    <div className="App">
-    <Card></Card>
-       
-    </div>
-);
+import React , {useState} from "react";
+
+
+
+
+function App(props)
+{
+    const [count,setCount]=useState(0);
+    const [count1,setCount1]=useState(0);
+    const [count2,setCount2]=useState(0);
+    return(
+        <div className="App">
+            <button onClick={() => {
+                // const newCount=count+1;
+                setCount(count+1);
+                // console.log(count);
+            }
+            }>{count}</button>
+
+<button onClick={() => {
+                // const newCount1=count1+1;
+                setCount1(count1+1);
+                // console.log(count1);
+            }
+            }>{count1}</button>
+
+
+
+<button onClick={() => {
+                // const newCount1=count1+1;
+                setCount2(count2+1);
+                // console.log(count1);
+            }
+            }>{count2}</button>
+        </div>
+
+    );
 }
 export default App;
